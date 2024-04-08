ff285f9c962ab2e911997a38266604108dd48857
Yuliia <kotyuliya24@gmail.com>
2024-04-08 19:35:23 -0400

    task1

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,4 @@
 'use strict';
 
 const categoriesTotal = document.querySelectorAll(".item");
-console.log(`Number of categories: ${categoriesTotal}`);
+console.log(`Number of categories: ${categoriesTotal.length}`);

1fc5e8e09ac5507aef0ff953403c43e0ac3449eb
Yuliia <kotyuliya24@gmail.com>
2024-04-08 19:43:33 -0400

    task1

--- js/task-1.js
+++ js/task-1.js
@@ -2,3 +2,12 @@
 
 const categoriesTotal = document.querySelectorAll(".item");
 console.log(`Number of categories: ${categoriesTotal.length}`);
+
+const categoryName = document.querySelectorAll("h2");
+
+function getCategName(categoryName) {
+
+  categoryName.forEach(function (item) {
+    console.log(`Category: ${item}`);
+  });
+}
